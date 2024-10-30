eec83eb82ab31547bf50c55bc047a4f98a5dc116
Raymond Fong How <raymondfonghow@gmail.com>
2024-10-29 20:57:54 -0400

    publish test 2

--- log.py
+++ log.py
@@ -1,25 +1,16 @@
-print('\n'*5)
-
 import logpy
 from logpy import gitcmd, pageworks
-
-print('\n'*5)
 
 # sess = gitcmd.Log()
 # sess.add_remark('General update.')
 # print('\n'*5)
 
-page = pageworks.Page('index')
-brics = page.read_brics()
-# for bric in brics:
-#     print(bric)
-# print('\n'*5)
+index = pageworks.Page('index')
+index.replace_brics(mode='preview')
+index.replace_brics(mode='publish')
 
-
-page.replace_brics(mode='publish')
-# for chunk in page.bric_split():
-#     print(chunk)
-print('\n'*5)
+dashboard = pageworks.Page('dashboard')
+dashboard.replace_brics(mode='preview')
 
 
 # sess.close()
